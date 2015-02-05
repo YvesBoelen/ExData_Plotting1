d725d6bae088abfd2408aa9fb6e0a118e0a47972
YvesBoelen <YvesBoelen@users.noreply.github.com>
2015-02-05 21:05:12 +0100

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,41 @@
+#store langugage settings
+user_lang <- Sys.getlocale("LC_TIME")
+Sys.setlocale("LC_TIME", "English")
+
+# read txt file and convert Date to a date variable
+epower <- read.table("./04_Exploratory_Data_Analysis/assignment1/household_power_consumption.txt",
+                     sep=";", header=TRUE, dec=".")
+epower$Date <- as.Date(epower$Date,"%d/%m/%Y")
+
+# Extract data for 2007-02-01 and 2007-02-02
+
+epower07 <- subset(epower,epower$Date %in% as.Date(c("2007-02-01","2007-02-02"),"%Y-%m-%d"))
+
+# Construct a datetime variable 
+epower07$datetimestring <- paste(epower07$Date, epower07$Time)
+epower07$datetimestring <- strptime(epower07$datetimestring,format = "%Y-%m-%d %H:%M:%S")
+
+# transform factor variables into numeric variables
+
+epower07$Global_active_power <- as.character(epower07$Global_active_power)
+epower07$Global_active_power <- as.numeric(epower07$Global_active_power)
+epower07$Global_reactive_power <- as.character(epower07$Global_reactive_power)
+epower07$Global_reactive_power <- as.numeric(epower07$Global_reactive_power)
+epower07$Voltage <- as.character(epower07$Voltage)
+epower07$Voltage <- as.numeric(epower07$Voltage)
+epower07$Sub_metering_1 <- as.character(epower07$Sub_metering_1)
+epower07$Sub_metering_1 <- as.numeric(epower07$Sub_metering_1)
+epower07$Sub_metering_2 <- as.character(epower07$Sub_metering_2)
+epower07$Sub_metering_2 <- as.numeric(epower07$Sub_metering_2)
+epower07$Sub_metering_3 <- as.character(epower07$Sub_metering_3)
+epower07$Sub_metering_3 <- as.numeric(epower07$Sub_metering_3)
+
+# Plot 2
+
+png(filename = "plot2.png", width=480, height=480)
+plot(epower07$datetimestring,epower07$Global_active_power,type='l', xlab = '',
+     ylab = 'Global Active Power (kilowatts)', main='')
+dev.off()
+
+#restore language
+Sys.setlocale("LC_TIME", user_lang)Enter file contents here
